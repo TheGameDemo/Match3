069cd410e24c7a7f8f325a84d537ec234015399a
HaiBooLang <53350622+HaiBooLang@users.noreply.github.com>
2023-07-07 23:18:59 +0800

    Avoiding Immediate Matches

--- Assets/Scripts/Match3Game.cs
+++ Assets/Scripts/Match3Game.cs
@@ -34,7 +34,44 @@ public class Match3Game : MonoBehaviour
         {
             for (int x = 0; x < size.x; x++)
             {
-                grid[x, y] = (TileState)Random.Range(1, 8);
+                // To Avoid Immediate Matches
+                TileState a = TileState.None, b = TileState.None;
+                int potentialMatchCount = 0;
+                if (x > 1)
+                {
+                    a = grid[x - 1, y];
+                    if (a == grid[x - 2, y])
+                    {
+                        potentialMatchCount = 1;
+                    }
+                }
+                if (y > 1)
+                {
+                    b = grid[x, y - 1];
+                    if (b == grid[x, y - 2])
+                    {
+                        potentialMatchCount += 1;
+                        if (potentialMatchCount == 1)
+                        {
+                            a = b;
+                        }
+                        else if (b < a)
+                        {
+                            (a, b) = (b, a);
+                        }
+                    }
+                }
+
+                TileState t = (TileState)Random.Range(1, 8 - potentialMatchCount);
+                if (potentialMatchCount > 0 && t >= a)
+                {
+                    t += 1;
+                }
+                if (potentialMatchCount == 2 && t >= b)
+                {
+                    t += 1;
+                }
+                grid[x, y] = t;
             }
         }
     }
